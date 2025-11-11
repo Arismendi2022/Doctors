b7451e060473320a5e28bbc469fee1c024a5ea36
Arismendi2022 <arismendi.guiza@gmail.com>
2025-11-11 07:47:45 -0500

    Create Patient Login and Registration system

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,46 +1,51 @@
 import { createContext, useEffect, useState } from "react";
-import {doctors} from "../assets/assets";
+import { doctors } from "../assets/assets";
 import axios from "axios";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-  const currencySymbol = '$'
-  const backendUrl = import.meta.env.VITE_BACKEND_URL
-  const [doctors, setDoctors] = useState([])
+	const currencySymbol = '$'
+	const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-  const value = {
-    doctors,
-    currencySymbol
-  }
+	const [doctors, setDoctors] = useState([])
+	const [token, setToken] = useState(localStorage.getItem("token") ? localStorage.getItem("token") : false);
 
-  const getDoctorsData = async () => {
 
-    try {
-      const {data} = await axios.get(backendUrl + '/api/doctor/list');
-      if (data.success) {
-        setDoctors(data.doctors);
-      } else {
-        toast.error(data.message);
-      }
+	const getDoctorsData = async () => {
 
-    } catch (error) {
-      console.error(error)
-      toast.error(error.message);
-    }
-  }
+		try {
+			const {data} = await axios.get(backendUrl + '/api/doctor/list');
+			if (data.success) {
+				setDoctors(data.doctors);
+			} else {
+				toast.error(data.message);
+			}
 
-  useEffect(() => {
-    getDoctorsData()
-  },[])
+		} catch (error) {
+			console.error(error)
+			toast.error(error.message);
+		}
+	}
 
-  return (
-    <AppContext.Provider value={value}>
-      {props.children}
-      </AppContext.Provider>
-  )
+	const value = {
+		doctors,
+		currencySymbol,
+		token, setToken,
+		backendUrl
+	}
+
+	useEffect(() => {
+		getDoctorsData()
+	}, [])
+
+	return (
+		<AppContext.Provider value={value}>
+			{props.children}
+		</AppContext.Provider>
+	)
 
 }
 
