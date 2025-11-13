a15fc66aeb27984b90d4c74d4d42b417833f6409
Arismendi2022 <arismendi.guiza@gmail.com>
2025-11-13 12:26:55 -0500

    Display and update patient profile data on website

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,8 @@
 import validator from 'validator';
 import bcrypt from 'bcrypt';
-import userModel from "../models/userModel.js";
+import userModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
+import { v2 as cloudinary } from 'cloudinary';
 
 
 // API to register user
@@ -65,7 +66,7 @@ const loginUser = async (req, res) => {
 		const isMatch = await bcrypt.compare(password, user.password);
 
 		if (isMatch) {
-			const token = jwt.sign({id:user._id}, process.env.JWT_SECRET);
+			const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
 			res.json({success: true, token});
 		} else {
 			res.json({success: false, message: "Invalid credentials."});
@@ -78,5 +79,55 @@ const loginUser = async (req, res) => {
 	}
 }
 
+// Api to get user profile data
+const getProfile = async (req, res) => {
 
-export { registerUser,loginUser };
+	try {
+
+		//const {userId} = req.body
+		const userId = req.user.userId;
+		const userData = await userModel.findById(userId).select('-password')
+
+		res.json({success: true, userData});
+
+	} catch (error) {
+		console.log(error)
+		res.json({success: false, message: error.message});
+	}
+}
+
+// Api to update user profile
+const updateProfile = async (req, res) => {
+
+	try {
+
+		const userId = req.user.userId; // Obtener del middleware authUser
+		const {name, phone, address, dob, gender} = req.body;
+
+		const imageFile = req.file
+
+		if (!name || !phone || !dob || !gender) {
+			return res.json({success: false, message: "Data Missing"});
+		}
+
+		await userModel.findByIdAndUpdate(userId, {name, phone, address: JSON.parse(address), dob, gender})
+
+		if (imageFile) {
+
+			// uploas image to cloudinary
+			const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: 'image'});
+			const imageURL = imageUpload.secure_url;
+
+			await userModel.findByIdAndUpdate(userId, {image: imageURL})
+		}
+
+		res.json({success: true, message: 'Profile updated.'})
+
+	} catch (error) {
+		console.log(error)
+		res.json({success: false, message: error.message});
+	}
+}
+
+
+export { registerUser, loginUser, getProfile, updateProfile };
